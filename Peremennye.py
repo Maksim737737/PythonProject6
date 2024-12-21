458f434fb859f3d5db346671c2de7da077634f33
Maksim737737 <maksksks37@gmail.com>
2024-12-21 20:14:39 +1100

    Initial commit

---
+++ Peremennye.py
@@ -0,0 +1,17 @@
+first_name = 'chasa potrebovalos na'
+second_name= ' domashnih zadani , po'
+third_name = 'minut na kazdoe.'
+name=1.5    # часа на 12 ДЗ
+mane=12     # шт ДЗ
+print(name/mane)
+print((name*60.0)*0.125) # минут на одно ДЗ
+weve=11.25
+print(name,first_name,mane,second_name,weve,third_name)
+a=name
+b=mane
+c=weve
+print(a,first_name,b,second_name,c,third_name)
+d=first_name
+e=second_name
+z=third_name
+print(a,d,b,e,c,z)
